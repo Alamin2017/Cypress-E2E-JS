21a335f0c9f4fb356d3fbed92d28dfb161ccf4dc
Al-Amin <alamincse12@gmail.com>
2024-12-14 00:38:59 +0600

    first commit

---
+++ cypress/e2e/Test_case_4.cy.js
@@ -0,0 +1,7 @@
+describe('Color code test automation', () => {
+    it('test started', () => {
+        cy.visit("https://www.skitto.com/");
+        cy.xpath("//ul[@class='secondary_menu']//a[normalize-space()='contact us']").click();
+        
+    });
+});
---
+++ cypress/pageobject/PageObject.js
@@ -0,0 +1,13 @@
+import HomeLocator from "../pages/HomePage";
+import ProductLocator from "../pages/ProductPage";
+class PageObject{
+    constructor(){
+        this.homeObj=new HomeLocator();
+        this.productObj=new ProductLocator();
+        
+    }
+}
+export default new PageObject;
+
+
+
