debae16a8c548fdae020e6df19903757f9b1f223
Al-Amin <alamincse12@gmail.com>
2025-03-03 20:13:06 +0600

    updated

--- cypress/e2e/Test_case_5.cy.js
+++ cypress/e2e/Test_case_5.cy.js
@@ -27,3 +27,16 @@ it('should login successfully with valid credentials', () => {
     cy.get("#log-in").click();
     cy.url().should("not.include", "/index.html");
 });
+// cy.xpath("//div[@class='alert alert-danger mt5 ng-star-inserted'] | //p[contains(text(),'We are unable to access your account due to an inv')]").should('be.visible').then(($msg) => {
+//       const errorMsg = $msg.text().trim();
+//       if (errorMsg === "Verification Code is invalid") {
+//         cy.log("Test is completed");
+//       } 
+//       else if (errorMsg === "We are unable to access your account due to an invalid verification code. Please contact Participant Services at 888.333.6315, option 1.") {
+//         cy.Unlock_LOCK_Status();
+//       } 
+//       else {
+//         cy.log("Unknown error message: " + errorMsg);
+//       }
+// });
+  
