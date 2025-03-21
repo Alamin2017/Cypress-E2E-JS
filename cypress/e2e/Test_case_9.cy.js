340b8b2ad282892ac27decc2b197878d4f80bd8e
Al-Amin <alamincse12@gmail.com>
2025-03-21 23:11:38 +0600

    updated

---
+++ cypress/e2e/Test_case_9.cy.js
@@ -0,0 +1,25 @@
+describe('Cypress Session Example', () => {
+
+  beforeEach(() => {
+    cy.session('loginSession', () => {
+      cy.clearCookies();
+      cy.clearLocalStorage();
+      cy.window().then((win)=> win.sessionStorage.clear());
+      cy.visit('https://automationexercise.com/login');
+      cy.xpath("//input[@data-qa='login-email']").type('tania1212@gmail.com');
+      cy.xpath("//input[@placeholder='Password']").type('tania1212');
+      cy.xpath("//button[normalize-space()='Login']").click();
+    });
+    // Ensure we are on the homepage after session restoration
+    cy.visit('https://automationexercise.com/');
+  });
+
+  it('Test Case 1: Verify Features Items', () => {
+    cy.xpath("//h2[normalize-space()='Features Items']").should('be.visible');
+  });
+
+  it('Test Case 2: Scroll to Bottom', () => {
+    cy.xpath("//a[normalize-space()='Logout']").click();
+  });
+
+});
